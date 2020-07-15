8e3fd02a3a83cacf419d66819d25b324c835f7ae
mrgnhnt96 <mrgnhnt96@gmail.com>
2020-07-15 06:58:31 -0700

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:blur_widget/presentation/home_screen.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,60 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(title: 'Blur Widget Demo'),
+      home: MyHomePage(title: 'Blur Widget Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 7, sigmaY: 7),
+            child: Container(
+              height: 100,
+              color: Colors.transparent,
+            ),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, i) {
+          return Container(
+            margin: const EdgeInsets.all(16),
+            height: 50,
+            color: Colors.red,
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        clipBehavior: Clip.hardEdge,
+        backgroundColor: Colors.blue.withOpacity(.6),
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 7, sigmaY: 7),
+            child: Center(child: Icon(Icons.add)),
+          ),
+        ),
+      ),
     );
   }
 }
